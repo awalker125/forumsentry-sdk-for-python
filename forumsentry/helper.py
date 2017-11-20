f3fa0ab1c2141c52fb41a1fd12b3a9c122e71e90
Andrew Walker <awalker125@users.noreply.github.com>
2017-11-20 09:30:48 +0000

    testing

---
+++ forumsentry/helper.py
@@ -0,0 +1,22 @@
+'''
+Created on 20 Nov 2017
+
+@author: walandre
+'''
+
+class Helper(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+    def stark(self):
+        return "Winter is comming"
+    
+    def lanister(self):
+        return "A Lanister always pays his debts"
