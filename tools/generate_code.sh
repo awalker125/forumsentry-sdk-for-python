f2972e4bc6297f7fb03806151c3683e0db3b9a42
Andrew Walker <awalker125@users.noreply.github.com>
2017-12-05 11:03:30 +0000

    http stuff

--- tools/generate_code.sh
+++ tools/generate_code.sh
@@ -2,9 +2,9 @@
 
 set -x 
 
-export SWAGGER_CODEGEN_VERSION=2.2.3
+#export SWAGGER_CODEGEN_VERSION=2.2.3
 #export SWAGGER_CODEGEN_VERSION=3.0.0-20170924.102354-5
-#export SWAGGER_CODEGEN_VERSION=2.3.0-20171129.164117-307
+export SWAGGER_CODEGEN_VERSION=2.3.0-20171129.164117-307
 
 export WHEREAMI=$(dirname $0)
 
@@ -43,9 +43,9 @@ then
 	echo "using cached swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar"
 else
 
-	#wget --no-check-certificate https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/2.3.0-SNAPSHOT/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar
+	wget --no-check-certificate https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/2.3.0-SNAPSHOT/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar
 	#wget --no-check-certificate https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/3.0.0-SNAPSHOT/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar
-	wget --no-check-certificate https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/${SWAGGER_CODEGEN_VERSION}/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar
+	#wget --no-check-certificate https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/${SWAGGER_CODEGEN_VERSION}/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar
 	echo "/swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar" >> ${WHEREAMI}/.gitignore 
 fi
 
@@ -55,10 +55,9 @@ rm -rf ${WHEREAMI}/_build
 mkdir ${WHEREAMI}/_build
 
 
-
 java -jar \
 	-Dmodels -DmodelDocs=false \
-	-DsupportingFiles=configuration.py,__init__.py,rest.py,api_client.py \
+	-DsupportingFiles=__init__.py \
 	swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar generate \
 	--output ${WHEREAMI}/_build \
 	--config ${WHEREAMI}/config.json \
@@ -66,27 +65,25 @@ java -jar \
 	-l python --auth "Authorization: Basic ${AUTH}" \
 	--template-dir ${WHEREAMI}/templates/python	
 
-
 # java -jar \
+	# -Dmodels -DmodelDocs=false \
+	# -DsupportingFiles=configuration.py,__init__.py,rest.py,api_client.py \
 	# swagger-codegen-cli-${SWAGGER_CODEGEN_VERSION}.jar generate \
 	# --output ${WHEREAMI}/_build \
 	# --config ${WHEREAMI}/config.json \
 	# -i ${REST_API_PROTOCOL}://${REST_API_HOST}:${REST_API_PORT}/restApi/v1.0/api-docs \
 	# -l python --auth "Authorization: Basic ${AUTH}" \
-	# --template-dir ${WHEREAMI}/templates/python		
+	# --template-dir ${WHEREAMI}/templates/python
+
 
 
 
 	
-if [ "$1" == "update" ]
-then
-	set -x
-	rm -rf ${WHEREAMI}/../forumsentry_api/
-	rm -rf ${WHEREAMI}/../tests/forumsentry_api/
-
-	#mkdir -p ../forumsentry_api
-	#mkdir -p ../test/forumsentry_api
-	
-	cp -R -p ${WHEREAMI}/_build/forumsentry_api ../
-	cp -R -p ${WHEREAMI}/_build/test/ ../tests/forumsentry_api
-fi
+# if [ "$1" == "update" ]
+# then
+	# set -x
+	# rm -rf ${WHEREAMI}/../forumsentry_api/
+	# rm -rf ${WHEREAMI}/../tests/forumsentry_api/
+	# cp -R -p ${WHEREAMI}/_build/forumsentry_api ../
+	# cp -R -p ${WHEREAMI}/_build/test/ ../tests/forumsentry_api
+# fi
